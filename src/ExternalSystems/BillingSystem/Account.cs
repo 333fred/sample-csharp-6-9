4abb7634cdeb902e5dc9093a2a037d7c1d25ad14
Kathleen Dollard <kdollard@microsoft.com>
2020-12-01 07:22:36 -0800

    Moved GenerateTestLicense and added w/oStringBuilder version

--- src/ExternalSystems/BillingSystem/Account.cs
+++ src/ExternalSystems/BillingSystem/Account.cs
@@ -8,7 +8,6 @@ namespace TollCollectorLib.BillingSystem
 {
     public class Account
     {
-        private static readonly Random _random = new();
 
         public Account(string license, Owner owner)
         {
@@ -22,52 +21,6 @@ namespace TollCollectorLib.BillingSystem
 
         public string State => License[^2..];
 
-        public static string GenerateTestLicense()
-        {
-            var states = new string[] { "BC", "CA", "ID", "OR", "WA" };
-
-            var builder = new StringBuilder();
-            var numberLength = _random.Next(4, 8);
-
-            for (int i = 0; i < numberLength; i++)
-            {
-                RandomAlphaNumeric(builder);
-            }
-
-            builder.Append('-');
-
-            builder.Append(states[_random.Next(1, states.Length) - 1]);
-
-            return builder.ToString();
-
-            static void RandomAlphaNumeric(StringBuilder builder)
-            {
-                if (Convert.ToBoolean(_random.Next(0, 2)))
-                {
-                    builder.Append((char)('0' + _random.Next(0, 10)));
-                }
-                else
-                {
-                    builder.Append((char)('A' + _random.Next(0, 26)));
-                }
-            }
-        }
-
-        public static string GenerateTestLicenseViaLinq()
-        {
-            var states = new string[] { "BC", "CA", "ID", "OR", "WA" };
-
-            var range = Enumerable.Range(0, _random.Next(4, 8));
-            return string.Join("", range.Select(x => RandomAlphaNumeric()))
-                      + $"-{states[_random.Next(1, states.Length) - 1]}";
-
-            static string RandomAlphaNumeric()
-            {
-                return Convert.ToBoolean(_random.Next(0, 2))
-                    ? ((char)('0' + _random.Next(0, 10))).ToString()
-                    : ((char)('A' + _random.Next(0, 26))).ToString();
-            }
-        }
     }
 }
 
--- src/ExternalSystems/BillingSystem/AccountList.cs
+++ src/ExternalSystems/BillingSystem/AccountList.cs
@@ -9,6 +9,7 @@ namespace TollCollectorLib.BillingSystem
     public class AccountList
     {
 
+        private static readonly Random _random = new();
         private Dictionary<string, Account> accounts;
 
         private AccountList()
@@ -16,7 +17,7 @@ namespace TollCollectorLib.BillingSystem
         }
 
 
-        public IEnumerable<Account> GetAccounts() 
+        public IEnumerable<Account> GetAccounts()
             => accounts.Select(x => x.Value);
 
         public static AccountList FetchAccounts(string countyName)
@@ -45,6 +46,72 @@ namespace TollCollectorLib.BillingSystem
                 return account;
             }
             throw new NotImplementedException();
+        }
+
+        public static string GenerateTestLicense()
+        {
+            var states = new string[] { "BC", "CA", "ID", "OR", "WA" };
+
+            var builder = new StringBuilder();
+            var numberLength = _random.Next(4, 8);
+
+            for (int i = 0; i < numberLength; i++)
+            {
+                RandomAlphaNumeric(builder);
+            }
+
+            builder.Append('-');
+
+            builder.Append(states[_random.Next(1, states.Length) - 1]);
+
+            return builder.ToString();
+
+            static void RandomAlphaNumeric(StringBuilder builder)
+            {
+                if (Convert.ToBoolean(_random.Next(0, 2)))
+                {
+                    builder.Append((char)('0' + _random.Next(0, 10)));
+                }
+                else
+                {
+                    builder.Append((char)('A' + _random.Next(0, 26)));
+                }
+            }
+        }
+
+        public static string GenerateTestLicenseithoutStringBuilder()
+        {
+            var states = new string[] { "BC", "CA", "ID", "OR", "WA" };
+
+            var numberLength = _random.Next(4, 8);
+            var s = "";
+            for (int i = 0; i < numberLength; i++)
+            {
+                s += RandomAlphaNumeric();
+            }
+
+            return $"{s}-{states[_random.Next(1, states.Length) - 1]}";
+
+            static string RandomAlphaNumeric()
+                => Convert.ToBoolean(_random.Next(0, 2))
+                    ? ((char)('0' + _random.Next(0, 10))).ToString()
+                    : ((char)('A' + _random.Next(0, 26))).ToString();
+        }
+
+        public static string GenerateTestLicenseViaLinq()
+        {
+            var states = new string[] { "BC", "CA", "ID", "OR", "WA" };
+
+            var range = Enumerable.Range(0, _random.Next(4, 8));
+            return string.Join("", range.Select(x => RandomAlphaNumeric()))
+                      + $"-{states[_random.Next(1, states.Length) - 1]}";
+
+            static string RandomAlphaNumeric()
+            {
+                return Convert.ToBoolean(_random.Next(0, 2))
+                    ? ((char)('0' + _random.Next(0, 10))).ToString()
+                    : ((char)('A' + _random.Next(0, 26))).ToString();
+            }
         }
     }
 }
